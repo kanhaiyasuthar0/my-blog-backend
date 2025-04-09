f76fd7a499f7c6d9830e9dfcd7ea1031afa86d04
Kanhaiya Suthar <kanhaiyasuthar@Kanhaiyas-MacBook-Pro.local>
2025-04-09 22:32:49 +0530

    new jwt

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,22 +1,20 @@
 // src/auth/jwt.strategy.ts
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, ExtractJwt } from 'passport-jwt';
-import * as dotenv from 'dotenv';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
-dotenv.config();
-console.log('Loaded JWT_SECRET from .env:1', process.env.JWT_SECRET);
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
   async validate(payload: any) {
-    console.log('Decoded JWT Payload:', payload);
-    return { userId: payload.id, email: payload.email };
+    return { userId: payload.sub, username: payload.username };
   }
 }
